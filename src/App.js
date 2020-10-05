7dcad85bc44438c186c2a18decd8d3dae9f1f95b
b1602001 <b1602001@ctu.edu.vn>
2020-10-05 15:10:13 +0700

    trang login

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Đăng nhập </h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
