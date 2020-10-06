99b3e385f1e30c8b41250fd281f5d1462df3b98c
b1602001 <b1602001@ctu.edu.vn>
2020-10-06 15:59:52 +0700

    Trang Login, và part Contact

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import './login.scss'
+import Login from './login'
+import Contact from './contact'
 
-Login.propTypes = {
-
+index.propTypes = {
+    
 };
 
-function Login(props) {
+function index(props) {
     return (
         <div>
-            <body>
-                <div class="login-clean">
-                    <form method="post">
-                        <div class="illustration"><i class="icon ion-ios-navigate"></i></div>
-                        <div class="form-group"><input type="email" class="form-control" name="email" placeholder="Email" /></div>
-                        <div class="form-group"><input type="password" class="form-control" name="password" placeholder="Password" /></div>
-                        <div class="form-group"><button class="btn btn-login btn-block" type="submit">Login</button></div>
-                        <a class="forgot" href="#">Forgot your email or password?</a>
-                        <div class="form-group"><button class="btn btn-create btn-block" type="submit">Create new Account</button></div>
-                    </form>
-                </div>
-            </body>
+            <Login />
+            <Contact />
         </div>
     );
 }
 
-export default Login;
+export default index;
---
+++ src/components/Login/login.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './login.scss'
+
+Login.propTypes = {
+
+};
+
+function Login(props) {
+    return (
+        <div>
+            <body>
+                <div class="login-clean">
+                    <form method="post">
+                        <p>
+                        
+                            "Khi bạn đăng nhập, mặc định bạn đồng ý với điêu khoản dịch vụ và chính sách bảo mật của Web"
+                            
+                        </p>
+                        <div class="illustration"><i class="icon ion-ios-navigate"></i></div>
+                        <div class="form-group"><input type="email" class="form-control" name="email" placeholder="Email" /></div>
+                        <div class="form-group"><input type="password" class="form-control" name="password" placeholder="Password" /></div>
+                        <div class="form-group"><button class="btn btn-login btn-block" type="submit">Login</button></div>
+                        <a class="forgot" href="#">Forgot your email or password?</a>
+                        <div class="form-group"><button class="btn btn-create btn-block" type="submit">Create new Account</button></div>
+                    </form>
+                </div>
+            </body>
+        </div>
+        
+    );
+}
+
+export default Login;
