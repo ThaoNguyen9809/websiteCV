7b48674bbdf8918f32fa348dd2d07e2b7b83621f
b1602001 <b1602001@ctu.edu.vn>
2020-10-07 15:53:27 +0700

    Trang Đăng ký

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
 import Login from './components/Login'
+import Registration from './components/Registration'
 
 function App() {
   return (
     <div className="App">
-      <h1> Đăng nhập </h1>
-      <Login />
+      {/* <h1> Đăng nhập </h1>
+      <Login /> */}
+      <Registration />
     </div>
   );
 }
